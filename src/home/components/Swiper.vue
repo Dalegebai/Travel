9a5d959442b65bb6738144654a17562f6c7b7bb4
zhengjiaqi <673932898@qq.com>
2020-07-30 10:29:52 +0800

    添加头部与轮播图

--- src/home/components/Swiper.vue
+++ src/home/components/Swiper.vue
@@ -1,10 +1,8 @@
 <template>
   <swiper ref="mySwiper" :options="swiperOptions">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
-    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.src" alt="test" />
+    </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 </template>
@@ -16,12 +14,21 @@ export default {
   data () {
     return {
       swiperOptions: {
-      }
+        loop: true
+      },
+      swiperList: [{
+        id: 1,
+        src: 'https://imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg'
+      }, {
+        id: 2,
+        src: 'https://imgs.qunarzz.com/vc/e3/a1/71/f498dfd3bed948d623c9093252.jpg_92.jpg'
+      }]
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .swiper-img
+    width: 100%
 </style>
